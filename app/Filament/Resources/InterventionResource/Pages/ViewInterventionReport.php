32d2ee4890fb0a5dbb6151d455a38887b4873961
Daniel Puigbo <puigbodaniel@gmail.com>
2026-01-11 18:33:38 +0100

    route fix

--- app/Filament/Resources/InterventionResource/Pages/ViewInterventionReport.php
+++ app/Filament/Resources/InterventionResource/Pages/ViewInterventionReport.php
@@ -9,7 +9,6 @@ use Filament\Resources\Pages\Page;
 class ViewInterventionReport extends Page
 {
     protected static string $resource = InterventionResource::class;
-
     protected static string $view = 'filament.resources.intervention-resource.pages.report';
 
     public Intervention $record;
@@ -17,12 +16,7 @@ class ViewInterventionReport extends Page
     public function mount(int|string $record): void
     {
         $this->record = Intervention::query()
-            ->with([
-                'system.controllerUnit.controllerModel',
-                'system.mechanicalUnits.robotModel',
-                'system.driveUnits.driveUnitModel',
-                'components.templateVersion.template',
-            ])
+            ->with(['system', 'components.templateVersion.template'])
             ->findOrFail($record);
     }
 }
